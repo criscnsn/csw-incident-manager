45c06f11d12cef5721b4b9ae84dbb7b3ff5f6623
criscnsn <santanacristopher765@gmail.com>
2026-09-10 14:52:19 -0600

    .

--- src/services/usecases/ListarIncidencias.java
+++ src/services/usecases/ListarIncidencias.java
@@ -3,10 +3,18 @@ package services.usecases;
 import models.Incidencia;
 import repository.IIncidenciaRepository;
 
+import java.util.List;
+
 public class ListarIncidencias {
-//    recuperación + formato de presentación
+
     private final IIncidenciaRepository<Incidencia> incidenciaRepository;
-    public ListarIncidencias(IIncidenciaRepository<Incidencia> incidenciaRepository){
+
+    public ListarIncidencias(final IIncidenciaRepository<Incidencia> incidenciaRepository) {
         this.incidenciaRepository = incidenciaRepository;
     }
+
+    public List<Incidencia> ejecutar() {
+        return this.incidenciaRepository.ListAll();
+    }
 }
+
