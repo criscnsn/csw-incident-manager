384c72d3224d30f9601360bc8958d20709a90902
criscnsn <santanacristopher765@gmail.com>
2026-09-09 20:15:21 -0600

    update a Incidencia

--- src/models/Incidencia.java
+++ src/models/Incidencia.java
@@ -1,11 +1,54 @@
 package models;
 
 public class Incidencia {
-    private int id;
-    private String equipo;
-    private String ubicacion;
-    private String descripcion;
-    private PrioridadIncidencia prioridad;
-    private EstadoIncidencia estado;
+    private final String identificador;
+    private final String equipoAfectado;
+    private final String ubicacionArea;
+    private final String descripcionFalla;
+    private final PrioridadIncidencia prioridad;
+    private EstadoIncidencia estadoActual;
 
+    public Incidencia(
+            String identificador,
+            String equipoAfectado,
+            String ubicacionArea,
+            String descripcionFalla,
+            PrioridadIncidencia prioridad,
+            EstadoIncidencia estadoActual
+    ) {
+        this.identificador = identificador;
+        this.equipoAfectado = equipoAfectado;
+        this.ubicacionArea = ubicacionArea;
+        this.descripcionFalla = descripcionFalla;
+        this.prioridad = prioridad;
+        this.estadoActual = estadoActual;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public String getEquipoAfectado() {
+        return equipoAfectado;
+    }
+
+    public String getUbicacionArea() {
+        return ubicacionArea;
+    }
+
+    public String getDescripcionFalla() {
+        return descripcionFalla;
+    }
+
+    public PrioridadIncidencia getPrioridad() {
+        return prioridad;
+    }
+
+    public EstadoIncidencia getEstadoActual() {
+        return estadoActual;
+    }
+
+    public void setEstadoActual(EstadoIncidencia nuevoEstado) {
+        this.estadoActual = nuevoEstado;
+    }
 }
