2bd2c76ede197874e48325346ea3f4c4c5ac4779
criscnsn <santanacristopher765@gmail.com>
2026-09-10 22:30:42 -0600

    refactor(usecases): simplify ListarIncidencias logic with unmodifiable list

--- src/services/usecases/ListarIncidencias.java
+++ src/services/usecases/ListarIncidencias.java
@@ -3,6 +3,7 @@ package services.usecases;
 import models.Incidencia;
 import repository.IIncidenciaRepository;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ListarIncidencias {
@@ -12,9 +13,15 @@ public class ListarIncidencias {
     public ListarIncidencias(final IIncidenciaRepository<Incidencia> incidenciaRepository) {
         this.incidenciaRepository = incidenciaRepository;
     }
-
+    /**
+     * Si hay incidencias registradas, devuelve una vista no modificable (Collections unmodifiableList)
+     * Si por alguna razón el repositorio fuera null, devuelve una Lista vacía
+     *
+     * NOTA: Si es muy obvio, pues borren esto PLOx
+     * */
     public List<Incidencia> ejecutar() {
-        return this.incidenciaRepository.ListAll();
+        final List<Incidencia> incidencias = this.incidenciaRepository.ListAll();
+        return (incidencias != null) ? Collections.unmodifiableList(incidencias) : List.of();
     }
 }
 
